7d8aab87070e96719eab8647d493c14af197fc6c
Dan V <dan@arp.io>
2013-11-17 22:31:03 +0800

    Updated Android.mk

--- android/jni/Android.mk
+++ android/jni/Android.mk
@@ -12,10 +12,16 @@ LOCAL_SRC_FILES := \
 	src/resin/cameras/PerspectiveCamera.cpp \
     src/resin/core/Attribute.cpp \
     src/resin/core/BufferGeometry.cpp \
+    src/resin/core/Image.cpp \
     src/resin/core/MemoryBuffer.cpp \
 	src/resin/core/Object3D.cpp \
-    src/resin/lights/Light.cpp \
+	src/resin/core/Stream.cpp \
+    src/resin/lights/AmbientLight.cpp \
     src/resin/lights/DirectionalLight.cpp \
+    src/resin/lights/HemisphereLight.cpp \
+    src/resin/lights/Light.cpp \
+    src/resin/lights/PointLight.cpp \
+    src/resin/materials/LineBasicMaterial.cpp \
     src/resin/materials/Material.cpp \
     src/resin/materials/MeshBasicMaterial.cpp \
     src/resin/materials/MeshPhongMaterial.cpp \
@@ -30,16 +36,20 @@ LOCAL_SRC_FILES := \
 	src/resin/math/Matrix4.cpp \
 	src/resin/math/Plane.cpp \
 	src/resin/math/Quaternion.cpp \
+	src/resin/math/Ray.cpp \
 	src/resin/math/Sphere.cpp \
 	src/resin/math/Triangle.cpp \
 	src/resin/math/Vector2.cpp \
 	src/resin/math/Vector3.cpp \
     src/resin/math/Vector4.cpp \
+    src/resin/objects/Line.cpp \
     src/resin/objects/Mesh.cpp \
     src/resin/renderers/GLES2Renderer.cpp \
     src/resin/renderers/Program.cpp \
+	src/resin/scenes/Fog.cpp \
 	src/resin/scenes/Scene.cpp \
-    src/resin/scenes/Fog.cpp \
+	src/resin/textures/Texture.cpp \
+    src/resin/thirdparty/stb_image/stb_image.c \
     src/resin/thirdparty/tinymt/tinymt32.c \
     src/resin/thirdparty/murmur/MurmurHash2.cpp
 
